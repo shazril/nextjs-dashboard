bda14447d5204e4daddc08e67a7d643f8c6385d7
Shazril Shohaimi <shazril@live.jp>
2023-10-29 14:44:16 +0800

    remove delay response

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,12 +20,12 @@ export async function fetchRevenue() {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    //console.log('Fetching revenue data...');
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch complete after 3 seconds.');
+    //console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -171,7 +171,7 @@ export async function fetchInvoiceById(id: string) {
     }));
 
     console.log(invoice); // Invoice is an empty array []
-    
+
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
